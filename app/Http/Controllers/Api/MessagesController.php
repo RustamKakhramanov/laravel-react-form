990457bce349e2c155d2ff82fc77655eda031010
rustam.kakhramanov <rustam_kakhramanov@auslogics.com>
2020-11-08 01:05:04 +0600

    Controller business logic
    * Post data validate
    * Save data to Model
    * Send to Email

--- app/Http/Controllers/Api/MessagesController.php
+++ app/Http/Controllers/Api/MessagesController.php
@@ -12,6 +12,28 @@ use Illuminate\Support\Facades\Mail;
 class MessagesController extends Controller
 {
     public function sendToEmail(Request $request) {
+        //Проверка данных
+        $request->validate([
+                'textMessage' => 'required|string|max:600'
+            ]
+        );
 
+        //Сохранение записи в бд || Save message in database
+        $message = Message::create([
+                'body' => $request->post('textMessage'),
+            ]
+        );
+
+        // We send a letter to the mail if the record is preserved || Отправляем письмо на почту если запись сохранилась
+        if( $message ) {
+            Mail::send('mails.default', ['text' => $message->body], function($message)
+            {
+                $message->to('foo@example.com', 'Какой то человек')->subject('Hi! This is message example');
+            });
+
+            return response()->json( $request->post('textMessage') );
+        }
+
+        return response()->json('Server Error', 500 );
     }
 }
